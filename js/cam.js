160f5e7842350c920278e419ac6cbc42bfe3ade1
Chris R <christophe.rast@heig-vd.ch>
2014-01-29 16:13:05 +0100

    facet okay !

--- js/cam.js
+++ js/cam.js
@@ -1,41 +1,49 @@
 $(function () {
-    // submit de la recherche
-    $('#globalSubmit').submit(researchGlobal);
+	// submit de la recherche
+	$('#globalSubmit').submit(researchGlobal);
+	getfacet('role');
+	hideSubFacet();
+	$('#role').on('change', changeFacet)
+	$('#role').on('endChangeFacet', function (event, field) {
+		if (field !== '') {
+			getSubfacet(field);
+		}
+	})
 })
 
 function researchGlobal() {
 
-    event.preventDefault();
+	event.preventDefault();
 
-    // récupère la valeur entrée dans l'input de recherche par exemple : person
-    var valueField = $('#globalSearch').val();
+	// récupère la valeur entrée dans l'input de recherche par exemple : person
+	var valueField = $('#globalSearch').val();
 
-    // test si le champ est rempli
-    if (valueField == '') {
-        alert('Please field the text area !');
-    } else {
-        // variable de l'url et de la requête
-        var url = "http://localhost:8983/solr/select";
-        var request = {};
+	// test si le champ est rempli
+	if (valueField == '') {
+		alert('Please field the text area !');
+	} else {
+		// variable de l'url et de la requête
+		var url = "http://localhost:8983/solr/select";
+		var request = {};
 
-        // les paramètres de l'objet request{}
-        request['q'] = 'global:' + valueField;
-        request['sort'] = 'id asc'; // a voir après 
-        request['rows'] = '10';
-        request['wt'] = 'json';
+		// les paramètres de l'objet request{}
+		request['q'] = 'global:' + valueField;
+		request['sort'] = 'id asc'; // a voir après 
+		request['rows'] = '10';
+		request['wt'] = 'json';
 
-        // la requête JSON dans global
-        $.getJSON(url, request, function (json) {
-            var result = json.response.docs
-            console.log(result)
-            $.each(result, function (i, e) {
-                buildHTMLDisplayResult(e)
-            })
-            //for (var i = 0; i < result.length; i++) {
-            //  buildHTMLDisplayResult(result[i]);
-            //}
-        });
+		// la requête JSON dans global
+		$.getJSON(url, request, function (json) {
+			var result = json.response.docs
+			console.log(result)
+			$.each(result, function (i, e) {
+				buildHTMLDisplayResult(e)
+			})
+			//for (var i = 0; i < result.length; i++) {
+			//  buildHTMLDisplayResult(result[i]);
+			//}
+		});
 
 
-    }
+	}
 }
---
+++ js/facet.js
@@ -0,0 +1,86 @@
+function getfacet(field) {
+	var url = "http://localhost:8983/solr/select";
+	$.getJSON(url, {
+			q: '*:*',
+			wt: 'json',
+			rows: '0',
+			facet: 'true',
+			'facet.field': field
+		},
+		function (json) {
+			var facets = getFacetsForField(json, field)
+			var select = undefined;
+			switch (field) {
+			case 'role':
+				select = $('#role')
+				break;
+			case 'person':
+				select = $('p.person select')
+				break;
+			case 'city.region.code':
+				select = $('p.city select')
+				break;
+			default:
+				select = $('p.media select')
+				break;
+			}
+			select.empty();
+			select.append($('<option>').val('').text('Choose...'))
+			facets.forEach(function (e, i) {
+				select.append($('<option>').val(e).text(e))
+			})
+
+		})
+
+}
+//needs a json full response as param, returns an array with the facetfields
+function getFacetsForField(json, field) {
+	var facet_field = new Array();
+	var array = json.facet_counts.facet_fields[field]
+	array.forEach(function (e, i) {
+		if (i % 2 == 0) {
+			facet_field.push(e)
+		}
+	})
+	return facet_field;
+}
+
+function hideSubFacet() {
+	$('p.subfacet').hide();
+}
+
+function changeFacet() {
+	var field = $('#role option:selected').val()
+	switch (field) {
+	case '':
+		$('p.subfacet').hide();
+		break;
+	case "person":
+		$('p.subfacet').hide();
+		$('p.person').fadeIn();
+		break;
+	case "city":
+		$('p.subfacet').hide();
+		$('p.city').fadeIn();
+		break;
+	default:
+		$('p.subfacet').hide();
+		$('p.media').fadeIn();
+		break;
+	}
+	$(this).trigger("endChangeFacet", [field])
+}
+
+function getSubfacet(field) {
+	switch (field) {
+	case 'city':
+		field = 'city.region.code'
+		break;
+	case 'person':
+		break;
+	default:
+		field = 'groupname'
+		break;
+	}
+	getfacet(field)
+}
